0b4248d99537d2ec5ab1c3bbee3fe9586f1ab80e
Olaolu Ajose <115143085+olaolu-tws@users.noreply.github.com>
2023-02-07 11:26:04 +0100

    #128 Descoping email & full name from the email request object. Authors: Caroline & Olaolu

--- blopup.notification/api/src/main/java/org/openmrs/module/blopup/notification/api/models/EmailRequest.java
+++ blopup.notification/api/src/main/java/org/openmrs/module/blopup/notification/api/models/EmailRequest.java
@@ -3,33 +3,12 @@ package org.openmrs.module.blopup.notification.api.models;
 import javax.validation.constraints.NotNull;
 
 public class EmailRequest {
-	
-	@NotNull(message = "From Email is mandatory")
-	String fromEmail;
-	
-	@NotNull(message = "Full Name is mandatory")
-	String fullName;
-	
+
 	String subject;
 	
 	@NotNull(message = "Content is mandatory")
 	String content;
-	
-	public String getFromEmail() {
-		return fromEmail;
-	}
-	
-	public void setFromEmail(String fromEmail) {
-		this.fromEmail = fromEmail;
-	}
-	
-	public String getFullName() {
-		return fullName;
-	}
-	
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
+
 	
 	public String getSubject() {
 		return subject;
